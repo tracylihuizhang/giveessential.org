433c6692aa45ec1a180e7e801985700ef787ad11
Tracy Lihui Zhang <tracylihuizhang@Tracys-MacBook-Pro-2.local>
2020-08-31 19:42:08 -0400

    logo height

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const LogoPic = styled.img`
-  height: 7vh;
+  height: 22vh;
   width: auto;
 `;
 
-export const ItalicText = styled.img`
-  textTransform: "uppercase",
-  fontFamily: "Archivo Narrow, sans-serif",
-  fontStyle: "normal",
-  fontWeight: "normal",
-`;
+// export const ItalicText = styled.img`
+//   textTransform: "uppercase",
+//   fontFamily: "Archivo Narrow, sans-serif",
+//   fontStyle: "normal",
+//   fontWeight: "normal",
+// `;
+
