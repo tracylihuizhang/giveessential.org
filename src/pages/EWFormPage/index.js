5b7b6baaacc62f2308d97a12d14e9e162a8ca9b3
Tracy Lihui Zhang <tracylihuizhang@Tracys-MacBook-Pro-2.local>
2020-09-01 00:09:27 -0400

    listening for screen dimension change

--- src/pages/EWFormPage/index.js
+++ src/pages/EWFormPage/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import {
   Screen,
   CenteredFlex,
@@ -19,6 +20,105 @@ import {
 import useStep from '../../hooks';
 import EWSubmitPage from '../EWSubmitPage';
 import EWMatchedPage from '../EWMatchedPage';
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//
+//     this.state = {
+//       height: 0,
+//       width: 0
+//     };
+//
+//     window.addEventListener("resize", this.update);
+//   }
+//
+//   componentDidMount() {
+//     this.update();
+//   }
+//
+//   update = () => {
+//     this.setState({
+//       height: window.innerHeight,
+//       width: window.innerWidth
+//     });
+//   };
+//
+//   render() {
+//     return (
+//         <React.Fragment>
+//           <p>height: {this.state.height}</p>
+//           <p>width: {this.state.width}</p>
+//         </React.Fragment>
+//     );
+//   }
+// }
+//
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+const getWidth = () => window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+const getHeight = () => window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+function useCurrentWidth() {
+  // save current window width in the state object
+  let [width, setWidth] = useState(getWidth());
+
+  // in this case useEffect will execute only once because
+  // it does not have any dependencies.
+  useEffect(() => {
+    // timeoutId for debounce mechanism
+    let timeoutId = null;
+    const resizeListener = () => {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId);
+      // change width from the state object after 150 milliseconds
+      timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+    };
+    // set resize listener
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, [])
+
+  return width;
+}
+
+function useCurrentHeight() {
+  let [height, setHeight] = useState(getHeight());
+
+  // in this case useEffect will execute only once because
+  // it does not have any dependencies.
+  useEffect(() => {
+    // timeoutId for debounce mechanism
+    let timeoutId = null;
+    const resizeListener = () => {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId);
+      // change width from the state object after 150 milliseconds
+      timeoutId = setTimeout(() => setHeight(getHeight()), 150);
+    };
+    // set resize listener
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, [])
+
+  return height;
+}
 
 export default function EssentialWorkerFormPage(props) {
   const [valueState, setValues] = useState([]);
@@ -48,6 +148,8 @@ export default function EssentialWorkerFormPage(props) {
   };
 
   // https://giveessential.uc.r.appspot.com/api/ew
+  let width = useCurrentWidth();
+  let height = useCurrentHeight();
 
   const renderStep = () => {
     switch (currentStep) {
@@ -55,6 +157,10 @@ export default function EssentialWorkerFormPage(props) {
         return (
           <Screen style={{ paddingBottom: 10 }}>
             <CenteredFlex>
+              <div>
+                {width} {height}
+                {/*{window.innerHeight} {window.innerWidth}*/}
+              </div>
               <Header>Essential Worker Form</Header>
               <Subtitle>The following questions ask about which items you need. 
                 We will pass this information along to your match so that they are able 
